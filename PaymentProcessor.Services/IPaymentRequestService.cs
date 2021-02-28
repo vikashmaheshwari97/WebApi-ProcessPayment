46a65f035e8b8377142df2af2e8fe839f6165b66
vikashmaheshwari97 <32438194+vikashmaheshwari97@users.noreply.github.com>
2021-02-28 16:54:19 +0500

    PaymentRequest Logic

---
+++ PaymentProcessor.Services/IPaymentRequestService.cs
@@ -0,0 +1,10 @@
+﻿using PaymentProcessor.Models.DTO;
+using System.Threading.Tasks;
+
+namespace PaymentProcessor.Services
+{
+    public interface IPaymentRequestService
+    {
+        Task<PaymentStateDto> Pay(PaymentRequestDto paymentRequestDto);
+    }
+}
